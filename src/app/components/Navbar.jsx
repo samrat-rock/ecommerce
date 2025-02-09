1699418a8a904f73588032d209fc6c88596d1b0f
samrat-rock <samratpandey715@gmail.com>
2025-02-09 14:56:53 +0545

    working on ecommerce

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
+import Menu from './Menu'
 
 function Navbar() {
   return (
-    <div className='h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64'>
-       
+    <div className='h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative'>
+       <div className='flex items-center justify-between h-full'>
+         
+      
+       {/*mobile */}
+
+       <Link href="/">
+       LAMA
+       <div className='text-2xl font-bold tracking-wide'>
+
+       </div>
+       </Link>
+       <Menu/>
+     
+       </div>
     </div>
   )
 }
