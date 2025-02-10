83d212a6db13dd89a6fa59162b4ac2378cd32a6a
samrat-rock <samratpandey715@gmail.com>
2025-02-10 14:24:53 +0545

    updated slider

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className={`py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-gray-100 text-sm mt-24  font-bold`}>
+    <div className={`py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-gray-100 text-sm mt-24 `}>
       {/* TOP */}
       <div className="flex flex-col md:flex-row justify-between gap-24">
         {/* LEFT */}
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,8 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 
 const slides = [
   {
@@ -30,12 +31,23 @@ const slides = [
   },
 ];
 
-function Slider() {
+const Slider = () => {
   const [current, setCurrent] = useState(0);
 
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+  //   }, 3000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
   return (
-    <div className="h-[calc(100vh-80px)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000">
+    <div className="h-[calc(100vh-80px)] ">
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
+      >
         {slides.map((slide) => (
           <div
             className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
@@ -43,22 +55,23 @@ function Slider() {
           >
             {/* TEXT CONTAINER */}
             <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
-              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                {slide.description}
+              </h2>
               <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
                 {slide.title}
               </h1>
               <Link href={slide.url}>
-                <button className="rounded-md bg-black text-white p-4 text-lg">
-                  Shop Now
+                <button className="rounded-md bg-black text-white py-3 px-4 ">
+                  SHOP NOW
                 </button>
               </Link>
             </div>
-
             {/* IMAGE CONTAINER */}
-            <div className="h-1/2 xl:w-1/2 xl:h-full relative min-h-screen flex items-center justify-center">
+            <div className="h-1/2 xl:w-1/2 xl:h-full relative">
               <Image
                 src={slide.img}
-                alt="Slide Image"
+                alt=""
                 fill
                 sizes="100%"
                 className="object-contain"
@@ -67,8 +80,38 @@ function Slider() {
           </div>
         ))}
       </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+        {slides.map((slide, index) => (
+          <div
+            className={`w-3 h-3  rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150" : ""
+            }`}
+            key={slide.id}
+            onClick={() => setCurrent(index)}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+        {
+            slides.map((slide, index)=>(
+                <div className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${current === index ? "scale-150": ""}`}
+                key={slide.id}
+                onClick={()=> setCurrent(index)}
+                >
+                       {current === index && (
+                        <div className="w-[6px] h-[6px] bg-gray-600 rounded-full">
+
+                        </div>)}
+                </div>
+            ))
+        }
+      </div>
     </div>
   );
-}
+};
 
 export default Slider;
